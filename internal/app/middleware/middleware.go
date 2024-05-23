677cb7e7c93effd8813cd218fb92150975ca1656
Andrey Bondar <a.y.bondar@gmail.com>
2024-05-23 16:23:29 +0300

    fix(middleware): linter be happy

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -23,7 +23,7 @@ func (r *loggingResponseWriter) Write(b []byte) (int, error) {
 	size, err := r.ResponseWriter.Write(b)
 	r.responseData.size += size
 
-	return size, err //nolint:wrapcheck
+	return size, err //nolint:wrapcheck // reimplement the interface and do not want to wrap the error
 }
 
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
