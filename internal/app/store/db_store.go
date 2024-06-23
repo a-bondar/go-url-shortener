13d342e6372a4ac0fe08d39a37c9e6de87983798
Andrey Bondar <a.y.bondar@gmail.com>
2024-06-23 20:18:08 +0300

    feat(db): add run migrations func on init

--- internal/app/store/db_store.go
+++ internal/app/store/db_store.go
@@ -2,10 +2,14 @@ package store
 
 import (
 	"context"
+	"embed"
 	"errors"
 	"fmt"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -16,12 +20,37 @@ type DBStore struct {
 }
 
 func newDBStore(ctx context.Context, dsn string) (*DBStore, error) {
+	if err := runMigrations(dsn); err != nil {
+		return nil, fmt.Errorf("failed to run DB migrations: %w", err)
+	}
+
 	pool, err := initPool(ctx, dsn)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create connection pool: %w", err)
 	}
 
 	return &DBStore{pool: pool}, nil
+}
+
+//go:embed migrations/*.sql
+var migrationsDir embed.FS
+
+func runMigrations(dsn string) error {
+	d, err := iofs.New(migrationsDir, "migrations")
+	if err != nil {
+		return fmt.Errorf("failed to return an iofs driver: %w", err)
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", d, dsn)
+	if err != nil {
+		return fmt.Errorf("failed to get a new migrate instance: %w", err)
+	}
+	if err := m.Up(); err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			return fmt.Errorf("failed to apply migrations to the DB: %w", err)
+		}
+	}
+	return nil
 }
 
 func initPool(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
