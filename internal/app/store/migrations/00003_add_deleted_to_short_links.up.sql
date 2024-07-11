226e9f960b3825c2edf58fc657bd89b402475330
Andrey Bondar <a.y.bondar@gmail.com>
2024-07-11 17:17:29 +0300

    feat(db): add unique_short_url_when_not_deleted index

--- internal/app/store/migrations/00003_add_deleted_to_short_links.up.sql
+++ internal/app/store/migrations/00003_add_deleted_to_short_links.up.sql
@@ -1,6 +1,13 @@
 BEGIN TRANSACTION;
 
 ALTER TABLE short_links
+    DROP CONSTRAINT short_links_short_url_key;
+
+ALTER TABLE short_links
     ADD COLUMN deleted BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE UNIQUE INDEX unique_short_url_when_not_deleted
+    ON short_links (short_url)
+    WHERE deleted = FALSE;
 
 COMMIT;
